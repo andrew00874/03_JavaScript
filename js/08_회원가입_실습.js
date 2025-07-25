6ab186737abb41b774ac6407e819d9f9896213bf
sosallycanwait <andrew00874@gmail.com>
2025-07-25 14:22:35 +0900

    회원가입 비밀번호 확인

--- js/08_회원가입_실습.js
+++ js/08_회원가입_실습.js
@@ -1,8 +1,15 @@
-<script>
-  // const 명칭 = password passwordConfirm passwordResult // keyup 을 이용해서
-  Enter 키 입력이 확인되면 // if 문을 활용해서 비밀번호가 일치하는지 확인하고,
-  // 비밀번호가 일치하면 비밀번호가 일치합니다! 와 함께 // 비밀번호 길이 : 글자
-  // 비밀번호 글자수 : 입력한 글자 수 // else 비밀번호가 일치하지 않는다면 //
-  원본비밀번호 : // 확인비밀번호 : 값 확인 // 응용편 // 비밀번호 가 "" 빈
-  값이라면 비밀번호를 입력하세요. 보여주기
-</script>;
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirm");
+const passwordResult = document.getElementById("password-result");
+
+passwordConfirm.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    if (password.value === "" || passwordConfirm.value === "") {
+      return;
+    } else if (password.value === passwordConfirm.value) {
+      passwordResult.innerHTML = "비밀번호가 일치합니다.";
+    } else {
+      passwordResult.innerHTML = "비밀번호가 불일치합니다.";
+    }
+  }
+});
